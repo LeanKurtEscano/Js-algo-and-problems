6793773d6aefc9e512024d94fb9cf082cccd15c8
Lean Kurt <leankurtescano@gmail.com>
2024-03-24 15:29:45 +0800

    async exercise in javascript

--- async.js
+++ async.js
@@ -1,23 +1,65 @@
-const wakeUp = (status) => {
-    return !status;
+
+
+
+const isWakeUp = (status) => {
+ return new Promise((resolve, reject) => {
+     setTimeout(() => {
+        resolve(!status);
+        return status;
+     }, 3000);
+ });
+    
 }
- const taskForToday = async() => {
-    let wokenUp = false;
-    let isWakeUp = wakeUp(wokenUp);
-    if(isWakeUp){
-        console.log("Person has woken up");
+
+const breakFast = async(status) => {
+   return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("done eating BreakFast");
+        resolve(!status);
+        return status;
+    }, 5000);
+
+   });
+}
+
+const takeABath = async(status) => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve("done taking a bath");
+            resolve(!status);
+        },5000);
+
+    });
+  
+}
+
+const dailyRoutine = async(status) => {
+    console.log("waking up..");
+    const wakeUp = false;
+    const doneEating = false;
+    const doneBath = false;
+    
+    const move =  await isWakeUp(wakeUp);
+    if(move) {
+        console.log("eating breakfast...");
+        const eatenBf = await breakFast(doneEating);
+        console.log(eatenBf);
+        if(eatenBf) {
+            console.log("taking a bath....");
+            const takingB = await takeABath(doneBath);
+            console.log(takingB);
+           return !status;
+        }
+    }
+
+};
+
+
+const dailyLife = async() => {
+    const done = await dailyRoutine();
+    if (done) {
+        console.log("Routine done for today");
     }
 }
 
-taskForToday();
-
-
-
-const stillSleeping = () => {
-
-}
-
-const eatBreakFast = () => {
-    console
-
-}
+dailyLife();
